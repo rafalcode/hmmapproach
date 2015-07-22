149c8a5c34c29b192242bf81972ac9a1c69d1546
Ramon Fallon <ramonfallon@gmail.com>
2015-07-22 23:57:17 +0200

    states files not being populated properly: please fix

--- genhmmda.c
+++ genhmmda.c
@@ -1,13 +1,3 @@
-/* from http://preshing.com/20111007/how-to-generate-random-timings-for-a-poisson-process
- *
- * This was about the number of earthquakes of Richter scale 4 or above every year throughout
- * the world as 13000. 365*1440/13000 is 40, so every 40 minutes there's an earthquake.
- *
- * Put another way, the average time between earthquakes is 40 minutes.
- *
- * To get the rate, we invert, so every 1 minute there's a 1/40 chance of an earthquake.
- *
- */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,25 +5,61 @@
 #include <sys/time.h>
 
 #define SETSEED 5 /*  if -DUNPREDRA is not fed in */
+#define DICENSIDES 6 /* number of sides to our dice */
+
+int chanceit(int n, int *iarr)
+{
+    int i;
+    float *arr=calloc(n, sizeof(float));
+
+    arr[0]= (float)iarr[0];
+    for(i=1;i<n;++i) 
+        arr[i]= arr[i-1] + (float)iarr[i];
+    for(i=0;i<n-1;++i) 
+        arr[i] /= arr[n-1];
+
+    /* this next step is entirely unnecessary, but it shows how the dice roll is being performed,
+     * in that, the final - summation - figure is not required */
+    arr=realloc(arr, (n-1)*sizeof(float)); /* lop off final value */
+#ifdef DBG
+    for(i=0;i<n-1;++i) 
+        printf("%f ", arr[i]); 
+    printf("\n"); 
+#endif
+
+    /* throw dice */
+    float ura= 1. - (float)random()/(RAND_MAX+1L); /* because we don't want to have zero, ever, for the log function */
+#ifdef DBG
+    printf("rndnum was %f\n", ura); 
+#endif
+
+    for(i=0;i<n;++i) {
+        if(ura>arr[i])
+            continue;
+        else
+            break;
+    }
+    free(arr);
+    return 1+i;
+}
 
 int main(int argc, char *argv[])
 {
-	if(argc!=4) {
-		printf("Error. Pls supply 3 arguments 1: Length untrained seq 2) length training data 3) avg numb of state change per training length.\n");
-		printf("Four file will be output: \"unkhmm.symb\", unkhmm.states\", \"trnhmm.symb\" and finally \"trnhmm.states\"");
-		exit(EXIT_FAILURE);
-	}
+    if(argc!=4) {
+        printf("Error. Pls supply 3 arguments 1) Length unknown seq 2) length training data 3) avg numb of state change per training length.\n");
+        printf("Four files will be output: \"unkhmm.symb\", unkhmm.states\", \"trnhmm.symb\" and finally \"trnhmm.states\"");
+        exit(EXIT_FAILURE);
+    }
     int tsqlen=atoi(argv[1]);
     int sqlen=atoi(argv[2]);
     int rem=tsqlen%sqlen;
-    if(!rem) {
-        printf("Sorry: one of the rules is that total unknown sequence length needs to be perfdctly\n");
+    if(rem) {
+        printf("Sorry: one of the rules is that total unknown sequence length needs to be perfectly\n");
         printf("divisible by the training sequence length\n"); 
-		exit(EXIT_FAILURE);
+        exit(EXIT_FAILURE);
     }
-    int times=tsqlen/sslen;
+    int times=tsqlen/sqlen;
 
-    float evplen=atof(argv[3]); /* events per length */
 #ifdef UNPREDRA
     struct timeval tnow;
     gettimeofday(&tnow, NULL);
@@ -44,18 +70,22 @@ int main(int argc, char *argv[])
     srand(SETSEED);
 #endif
 
+    float evplen=atof(argv[3]); /* events per length */
     float lam = evplen/sqlen;
     int i=0;
     float ura; /*  variable to hold one uniform random variable 0-1 */
     float wt /* waiting time */;
-    float twt=.0, oldtwt=.0;
+    float twt=.0;
+    /* hard-coded data now follows: */
     char cc[2]={'A', 'B'};
-    char die[2][6]={ {1,1,1,1,1,1}, {1,1,1,1,1,5} };
+    int die[2][DICENSIDES]={ {1,1,1,1,1,1}, {1,1,1,1,1,5} };
+    printf("throwndie=%i\n",chanceit(6,die[1]));
+    /* end of hardcoded data */
     unsigned char c=0;
 
-/* first we're going to populate our training data files */
-        FILE *ftsy/*fileptr train symbs*/=fopen("./trnhmm.symb", "w");
-        FILE *ftsta/*fileptr train states*/=fopen("./trnhmm.states", "w");
+    /* first we're going to populate our training data files */
+    FILE *ftsy/*fileptr train symbs*/=fopen("./trnhmm.symb", "w");
+    FILE *ftsta/*fileptr train states*/=fopen("./trnhmm.states", "w");
     while(1) { /* infinite loop to be broken out of */
         ura= 1. - (float)random()/(RAND_MAX+1L); /* because we don't want to have zero, ever, for the log function */
         wt= -log(ura)/lam;
@@ -65,21 +95,27 @@ int main(int argc, char *argv[])
 #endif
         while((i<=(int)twt) & (i<sqlen)) {
             fputc(cc[0x01&c], ftsy);
-            fputc(chanceit(die[0x01&c]), ftsta);
+            printf("%i ", chanceit(DICENSIDES, die[0x01&c]));
+            fputc(chanceit(DICENSIDES, die[0x01&c]), ftsta);
             i++;
         }
-        printf("\n"); 
         if(twt >= sqlen)
             break;
         c++;
     }
+    printf("\n"); 
+    fputc('\n', ftsy);
+    fputc('\n', ftsta);
     fclose(ftsy);
     fclose(ftsta);
 
-/* next we go for our unknown training dat files */
-		FILE *fusy/*fileptr unkpown symbols*/=fopen("./unkhmm.symb", "w");
-		FILE *fusta/*fileptr unknown states*/=fopen("./unkhmm.states", "w");
-        for(j=0;j<times;++j) {
+    /* next we go for our unknown training dat files */
+    FILE *fusy/*fileptr unkpown symbols*/=fopen("./unkhmm.symb", "w");
+    FILE *fusta/*fileptr unknown states*/=fopen("./unkhmm.states", "w");
+    c=0; /* yes, we start like we started the training sequence */
+    i=0; /* yes, we start like we started the training sequence */
+    twt=.0;
+    lam = times*evplen;
     while(1) { /* infinite loop to be broken out of */
         ura= 1. - (float)random()/(RAND_MAX+1L); /* because we don't want to have zero, ever, for the log function */
         wt= -log(ura)/lam;
@@ -87,16 +123,18 @@ int main(int argc, char *argv[])
 #ifdef DBG
         printf("Free up to %.2f: ", twt); 
 #endif
-        while((i<=(int)twt) & (i<sqlen)) {
-            fputc(cc[0x01&c], ftsy);
-            fputc(chanceit(die[0x01&c]), ftsta);
+        while((i<=(int)twt) & (i<tsqlen)) {
+            fputc(cc[0x01&c], fusy);
+            fputc(chanceit(DICENSIDES, die[0x01&c]), fusta);
             i++;
         }
-        printf("\n"); 
-        if(twt >= sqlen)
+        if(twt >= tsqlen)
             break;
         c++;
     }
-        }
+    fputc('\n', fusy);
+    fputc('\n', fusta);
+    fclose(fusy);
+    fclose(fusta);
     return 0;
 }
